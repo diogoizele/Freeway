7efbf382925307ca86996096ae1ad88e520ce15e
Diogo <diogogabrielizele@gmail.com>
2021-05-17 12:24:52 -0300

    first commit: images, index, sketch and preload added

---
+++ preload.js
@@ -0,0 +1,15 @@
+let car1Img,
+  car2Img,
+  car3Img,
+  car4Img,
+  car5Img,
+  car6Img,
+  car7Img,
+  car8Img,
+  cowImg,
+  highWayImg;
+
+function preload() {
+  car1 = loadImage("./assets/img/car-01.png");
+  highWayImg = loadImage("./assets/img/high-way.png");
+}
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(500, 400);
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  background(highWayImg);
+}
